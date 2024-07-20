5efea3b8f70c52536c8eb53d6310b53b843c07f5
gregory <gregorypinheiro88@gmail.com>
2024-07-20 14:13:16 -0300

    inserindo jquery e js

---
+++ main.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    $('#telefone').mask('(00) 00000-0000',{
+        placeholder: '(__) _____-____'
+    })
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email:true,
+            },
+            telefone: {
+                required:true
+            }
+        },
+        messages: {
+            nome: {
+                required: 'Por favor insira seu nome'
+            },
+            telefone: 'Insira seu telefone',
+            email: 'Insira seu email'
+        },
+        submitHandler: function(form) {
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.
+            numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }
+        }
+    });
+});
